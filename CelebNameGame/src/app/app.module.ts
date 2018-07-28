a0f3b4e238e3559fefb91508fde97c8a36784117
crq83 <crq83@wildcats.unh.edu>
2018-07-28 08:20:37 -0400

    html and css setup for search component

--- CelebNameGame/src/app/app.module.ts
+++ CelebNameGame/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
