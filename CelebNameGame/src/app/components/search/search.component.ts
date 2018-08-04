196e753f925a61def0b696e887af53a9010de82f
crq83 <crq83@wildcats.unh.edu>
2018-08-04 09:02:03 -0400

    Search component clears out results when there's no input and also displays a placeholder image if there is not valid image for the movie.

--- CelebNameGame/src/app/components/search/search.component.ts
+++ CelebNameGame/src/app/components/search/search.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms'
 import { SearchService } from '../search/search.service'
+import {EmptyObservable} from 'rxjs/observable/EmptyObservable';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'app-search',
@@ -12,11 +19,23 @@ export class SearchComponent implements OnInit {
   constructor(private _searchService: SearchService) {}
 
   results: any[] = [];
+  response: any;
   queryField: FormControl = new FormControl();
   ngOnInit() {
-    console.log('here');
-    console.log(this.queryField.value);
-    this.queryField.valueChanges.subscribe( result => console.log(result));
+    this.queryField.valueChanges
+      .debounceTime(200)
+      .switchMap(queryField =>
+        {
+          if(queryField.trim() != ""){
+            return this._searchService.search(queryField)
+          }else{
+            this.results = [];
+            return new EmptyObservable();
+          }
+        })
+      .subscribe(response => {
+        this.results = response.json().results
+        console.log(this.results);
+      });
   }
-
 }
--- CelebNameGame/src/app/components/search/search.service.ts
+++ CelebNameGame/src/app/components/search/search.service.ts
@@ -12,7 +12,7 @@ export class SearchService {
   constructor(private _http: Http) { }
 
   search(queryString: string){
-    let _URL = this.baseUrl + queryString;
+    let _URL = this.baseUrl + "\"" + queryString + "\"";
     return this._http.get(_URL);
   }
 
